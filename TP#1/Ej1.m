087e436866e0cd0cb194a2293aab2fe78a240335
Elena Irurueta <84137564+elenairurueta@users.noreply.github.com>
2022-03-15 14:35:42 -0300

    Update Ej1.m

---
+++ TP#1/Ej1.m
@@ -0,0 +1,25 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% Back - Substitution
+%%% Resolución hacia atrás de Sistemas de Ecuaciones Lineales Triangularizados Superiormente
+%%%
+%%% Parámetros de Entrada:
+%%%    A = Matriz Triangular Superior No Singular (ceros bajo diag. princ)
+%%%    B = Vector Independiente
+%%%
+%%% Parámetro de Salida:
+%%%    X = Vector Solución de AX = B
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function X = Back_Subs(A,B)
+
+if nargin<2
+    disp('Debe ingresar Martiz Tiangular Superior y Vector Independiente');
+    return;
+end
+
+N=size(A,1);
+X=zeros(N,1);     % Vector Resultado
+X(N)=B(N)/A(N,N);   % Valor inicial del resultado
+for k = N-1:-1:1
+    X(k)=(B(k)-A(k,k+1:N)*X(k+1:N))/A(k,k); 
+end
